83db806091e1af864a238e1e469bca3f53d03198
ZionForum <127395075+ZionForum@users.noreply.github.com>
2023-03-09 20:55:25 +0800

    Add files via upload

---
+++ Activity2/Activity2/Form1.Designer.vb
@@ -0,0 +1,115 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtnum1 = New System.Windows.Forms.TextBox()
+        Me.txtnum2 = New System.Windows.Forms.TextBox()
+        Me.btnsubmit = New System.Windows.Forms.Button()
+        Me.btnclear = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(32, 12)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(75, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Enter Number:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(32, 43)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(75, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Enter Number:"
+        '
+        'txtnum1
+        '
+        Me.txtnum1.Location = New System.Drawing.Point(124, 5)
+        Me.txtnum1.Name = "txtnum1"
+        Me.txtnum1.Size = New System.Drawing.Size(139, 20)
+        Me.txtnum1.TabIndex = 2
+        '
+        'txtnum2
+        '
+        Me.txtnum2.Location = New System.Drawing.Point(124, 36)
+        Me.txtnum2.Name = "txtnum2"
+        Me.txtnum2.Size = New System.Drawing.Size(139, 20)
+        Me.txtnum2.TabIndex = 3
+        '
+        'btnsubmit
+        '
+        Me.btnsubmit.BackColor = System.Drawing.SystemColors.Info
+        Me.btnsubmit.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnsubmit.Location = New System.Drawing.Point(64, 75)
+        Me.btnsubmit.Name = "btnsubmit"
+        Me.btnsubmit.Size = New System.Drawing.Size(75, 23)
+        Me.btnsubmit.TabIndex = 4
+        Me.btnsubmit.Text = "&Submit"
+        Me.btnsubmit.UseVisualStyleBackColor = False
+        '
+        'btnclear
+        '
+        Me.btnclear.BackColor = System.Drawing.SystemColors.Info
+        Me.btnclear.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnclear.Location = New System.Drawing.Point(155, 75)
+        Me.btnclear.Name = "btnclear"
+        Me.btnclear.Size = New System.Drawing.Size(75, 23)
+        Me.btnclear.TabIndex = 5
+        Me.btnclear.Text = "&Clear"
+        Me.btnclear.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.ClientSize = New System.Drawing.Size(305, 109)
+        Me.Controls.Add(Me.btnclear)
+        Me.Controls.Add(Me.btnsubmit)
+        Me.Controls.Add(Me.txtnum2)
+        Me.Controls.Add(Me.txtnum1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Determine the Number"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtnum1 As TextBox
+    Friend WithEvents txtnum2 As TextBox
+    Friend WithEvents btnsubmit As Button
+    Friend WithEvents btnclear As Button
+End Class
---
+++ Activity2/Activity2/Form1.vb
@@ -0,0 +1,24 @@
+﻿Public Class Form1
+    Private Sub btnclear_Click(sender As Object, e As EventArgs) Handles btnclear.Click
+        txtnum1.Clear()
+        txtnum2.Clear()
+        txtnum1.Select()
+        txtnum2.Select()
+    End Sub
+    Private num1, num2 As Integer
+    Private Sub btnsubmit_Click(sender As Object, e As EventArgs) Handles btnsubmit.Click
+        'input
+        num1 = Val(txtnum1.Text)
+        num2 = Val(txtnum2.Text)
+        'process and output
+        If num1 > num2 Then
+            MessageBox.Show("First number is larger than the Second number", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        ElseIf num2 > num1 Then
+            MessageBox.Show("Second number is larger than the First number", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Else
+            MessageBox.Show("Both Inputted Numbers are equal", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+        txtnum1.Select()
+        txtnum2.Select()
+    End Sub
+End Class
